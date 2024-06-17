2a04afc047ba54ae7a5d7198731ca1ecf7b1de90
Joao <jcardosokm@gmail.com>
2024-06-18 00:12:03 +0200

    init

--- tests/Unit/Factory/DefinitionFactoryTest.php
+++ tests/Unit/Factory/DefinitionFactoryTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace AttributeAutoRegisterBundle\Tests\Unit\Factory;
 
+use AttributeAutoRegisterBundle\Attribute\Autowired;
 use AttributeAutoRegisterBundle\Factory\DefinitionFactory;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\TestCase;
@@ -19,11 +20,31 @@ class DefinitionFactoryTest extends TestCase
         $this->factory = new DefinitionFactory();
     }
 
+    public function testCreateDefinitionFromAttribute(): void
+    {
+        $attribute = new Autowired(
+            'UT\Autowired',
+            'UT\Factory',
+            'UT\FactoryMethod',
+            ['UT\AutowiredAlias']
+        );
+
+        $definition = $this->factory->createFromAttribute($attribute);
+
+        static::assertSame($attribute->id, $definition->getClass());
+        static::assertSame([$attribute->factory, $attribute->factoryMethod], $definition->getFactory());
+        static::assertSame($attribute->aliases, $definition->getTags());
+        static::assertTrue($definition->isPublic());
+        static::assertTrue($definition->isAutowired());
+    }
+
     public function testCreateDefinition(): void
     {
         $namespace  = 'UT\Namespace';
         $definition = $this->factory->createFromNamespace($namespace);
 
         static::assertSame($namespace, $definition->getClass());
+        static::assertTrue($definition->isPublic());
+        static::assertTrue($definition->isAutowired());
     }
 }
