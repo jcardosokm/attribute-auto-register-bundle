5efb38df48e2611ca3e930ff086dc885e142cfdb
Joao <jcardosokm@gmail.com>
2024-06-17 23:51:10 +0200

    init

--- src/Attribute/Autowired.php
+++ src/Attribute/Autowired.php
@@ -5,15 +5,14 @@ declare(strict_types=1);
 namespace AttributeAutoRegisterBundle\Attribute;
 
 use Attribute;
-use PHPUnit\Framework\Attributes\CodeCoverageIgnore;
 
 #[Attribute(Attribute::TARGET_CLASS | Attribute::IS_REPEATABLE)]
 class Autowired
 {
     /**
+     * @codeCoverageIgnore
      * @param array<string> $aliases
      */
-    #[CodeCoverageIgnore]
     public function __construct(
         public readonly ?string $id = null,
         public readonly ?string $factory = null,
