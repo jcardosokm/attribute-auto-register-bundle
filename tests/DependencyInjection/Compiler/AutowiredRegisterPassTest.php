8545b1ba38eec90f59c16aefbe49952dfcf4fa70
Joao <jcardosokm@gmail.com>
2024-06-18 00:55:30 +0200

    init

---
+++ tests/DependencyInjection/Compiler/AutowiredRegisterPassTest.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AttributeAutoRegisterBundle\Tests\DependencyInjection\Compiler;
+
+use AttributeAutoRegisterBundle\DependencyInjection\Compiler\AutowiredRegisterPass;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Throwable;
+
+#[CoversClass(AutowiredRegisterPass::class)]
+class AutowiredRegisterPassTest extends TestCase
+{
+    private AutowiredRegisterPass $compilerPass;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->compilerPass = new AutowiredRegisterPass();
+    }
+
+    /**
+     * @throws Throwable
+     */
+    public function testProcess()
+    {
+        $container = $this->createMock(ContainerBuilder::class);
+        $this->compilerPass->process($container);
+
+        static::assertNotNull($container->getDefinitions());
+    }
+}
