ab0e8d724d04b1b47031c1c89dedbdde939be9b2
Joao <jcardosokm@gmail.com>
2024-06-23 23:31:02 +0200

    add tests

--- tests/Functional/App/Entity/TestClass.php
+++ tests/Functional/App/Entity/TestClass.php
@@ -6,21 +6,23 @@ namespace AttributeAutoRegisterBundle\Tests\Functional\App\Entity;
 
 use AttributeAutoRegisterBundle\Attribute\Autowired;
 use AttributeAutoRegisterBundle\Tests\Functional\App\TaggedServices\TaggedServiceInterface;
+use Exception;
+use IteratorAggregate;
 use Symfony\Component\DependencyInjection\Attribute\AutowireIterator;
-use Traversable;
 
 #[Autowired]
 class TestClass
 {
-    public function __construct(#[AutowireIterator('tagged_service')] private Traversable $taggedServices)
+    public function __construct(#[AutowireIterator('tagged_service')] private IteratorAggregate $taggedServices)
     {
     }
 
     /**
-     * @return array<TaggedServiceInterface>
+     * @return TaggedServiceInterface[]
+     * @throws Exception
      */
-    public function getTaggedServices(): iterable
+    public function getTaggedServices(): array
     {
-        return iterator_to_array($this->taggedServices);
+        return iterator_to_array($this->taggedServices->getIterator());
     }
 }
