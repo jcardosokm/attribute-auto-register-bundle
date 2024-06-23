d04fafffc0401629786eef475bdf068e35a3f6b9
Joao <jcardosokm@gmail.com>
2024-06-23 23:08:07 +0200

    add tests

--- tests/Unit/TestFactoryClass.php
+++ tests/Unit/TestFactoryClass.php
@@ -4,11 +4,11 @@ declare(strict_types=1);
 
 namespace AttributeAutoRegisterBundle\Tests\Unit;
 
-use App\Factory\FactoryClassFactory;
 use AttributeAutoRegisterBundle\Attribute\Autowired;
+use AttributeAutoRegisterBundle\Tests\Functional\App\Factory\FactoryClassFactory;
 
-#[Autowired (id: 'factory_class_a', factory: FactoryClassFactory::class, factoryMethod: 'createA')]
-#[Autowired (id: 'factory_class_b', factory: FactoryClassFactory::class, factoryMethod: 'createB')]
+#[Autowired(id: 'factory_class_a', factory: FactoryClassFactory::class, factoryMethod: 'createA')]
+#[Autowired(id: 'factory_class_b', factory: FactoryClassFactory::class, factoryMethod: 'createB')]
 class TestFactoryClass
 {
     public function __construct(private string $testConfig)
