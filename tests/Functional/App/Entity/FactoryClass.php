7e533cc8b3a40d07fd543896abaeb7239a008946
Joao <jcardosokm@gmail.com>
2024-06-24 22:01:32 +0200

    ìmplement attributeValidator

--- tests/Functional/App/Entity/FactoryClass.php
+++ tests/Functional/App/Entity/FactoryClass.php
@@ -7,8 +7,8 @@ namespace AttributeAutoRegisterBundle\Tests\Functional\App\Entity;
 use AttributeAutoRegisterBundle\Attribute\Autowired;
 use AttributeAutoRegisterBundle\Tests\Functional\App\Factory\FactoryClassFactory;
 
-#[Autowired (id: 'factory_class_a', factory: FactoryClassFactory::class, factoryMethod: 'createA')]
-#[Autowired (id: 'factory_class_b', factory: FactoryClassFactory::class, factoryMethod: 'createB')]
+#[Autowired(id: 'factory_class_a', factory: FactoryClassFactory::class, factoryMethod: 'createA')]
+#[Autowired(id: 'factory_class_b', factory: FactoryClassFactory::class, factoryMethod: 'createB')]
 class FactoryClass
 {
     public function __construct(private string $testConfig)
